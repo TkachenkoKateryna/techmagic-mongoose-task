0c0ec9ce537ecd8126f76833ba4f467eae776545
Kateryna Tkachenko <katetkacenko@gmail.com>
2023-05-15 17:32:21 +0300

    Fixed sort. Added postman collection.

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -4,6 +4,17 @@ export class APIFeatures {
 		this.queryString = queryString;
 	}
 
+	sort() {
+		if (this.queryString.sort) {
+			const sortBy = this.queryString.sort.split(",").join(" ");
+			this.query = this.query.sort(sortBy);
+		} else {
+			this.query = this.query.sort("age");
+		}
+
+		return this;
+	}
+
 	paginate() {
 		const page = this.queryString.page * 1 || 1;
 		const limit = this.queryString.limit * 1 || 100;
